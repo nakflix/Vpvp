699423624c423ba62c63bd59faf1a9c2e6b75300
nakflix <123237114+nakflix@users.noreply.github.com>
2023-01-21 13:10:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'GreyMatters_Bot'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://GITHUB-USERNAME:token@github.com/GITHUB-USERNAME/YOUR-REPO-NAME okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://nakflix: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@github.com/nakflix/File-sharing-Bot okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
